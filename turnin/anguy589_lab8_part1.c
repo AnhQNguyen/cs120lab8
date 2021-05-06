6acd24b7d79f4521edf49a0fcadcb77c7364a28e
AnhQNguyen <66961614+AnhQNguyen@users.noreply.github.com>
2021-05-05 23:40:07 -0700

    Update anguy589_lab8_part1.c

--- turnin/anguy589_lab8_part1.c
+++ turnin/anguy589_lab8_part1.c
@@ -6,6 +6,8 @@
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
+ *
+ *	Demo Link: https://drive.google.com/file/d/1y_AX_DLpsOP0g30BV84dctF089Gj8dRy/view?usp=sharing
  */
 #include <avr/io.h>
 #include <avr/interrupt.h>
